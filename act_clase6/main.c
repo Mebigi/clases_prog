fc260f19a3538ffbd95ad9589545cfc3a0070235
Antonella Marcolin <you@example.com>
2018-04-10 11:56:01 -0300

    complentado actividad

--- act_clase6/main.c
+++ act_clase6/main.c
@@ -12,13 +12,21 @@
 // converte a minuscula strlwr
 // converte a may  strupr
 
+
+
 int main()
 {
     char nombre[50];
     char apellido[50];
     char nombreyapellido[120];
     int len;
-    int len2;
+
+    int s = isdigit('2');
+    int d = isalpha('a');
+
+    //teara el usuario ingresa un numero una funcio que me diga si es numerico ono cero no es valido 1 es valido
+    //parese en c para trasformar en un entero
+
 
     //m a r i a  d e  l a  s     m  e  r  c  e  d  e  s  /0 b  i  g  i /0
     //0 1 2 3 45 6 78 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
@@ -33,48 +41,35 @@ int main()
     gets(apellido);
 
 
-    strcat(nombreyapellido, nombre);
+    strcpy(nombreyapellido, apellido); //evito mugre
 
     strcat(nombreyapellido, ", ");
 
-    strcat(nombreyapellido, apellido);
+    strcat(nombreyapellido, nombre);
 
+    strlwr(nombreyapellido);
 
-     printf(" %s\n", nombreyapellido);
+    nombreyapellido[0]=toupper(nombreyapellido[0]);
 
+    len = strlen(nombreyapellido);
 
-
-
-    len= 200;
-    len2 = (strlen(nombreyapellido));
-    printf("len %d\n", len);
-    printf("len2 %d\n", len2);
-
-
-    for (int i=0; i<len2; i++)
-     {
-
-    if (nombreyapellido[i] != '\0')
+    for(int i=1; i<len; i++)
     {
-        if (i < len2 && i > 0)
-       {
-            nombreyapellido[i] = tolower(nombre[i]);
-       }
-        if (i == 0)
-       {
-            nombreyapellido[i] = toupper(nombre[i]);
-       }
 
-    } else
-    {
-        nombreyapellido[i] = ' ';
+        //if (nombreyapellido[i-1] == ' ')
+        if(isspace(nombreyapellido[i]))
+
+         {
+             nombreyapellido[i+1]=toupper(nombreyapellido[i+1]);
+         }
+
+
 
     }
 
 
 
 
-     }
 
       puts(nombreyapellido);
 
