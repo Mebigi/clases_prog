ea0211bd601fa7ae7da50bb21785e1d9d8d87683
Mercedes Bigi <mebigi@hotmail.com>
2018-04-16 19:18:23 -0300

    clase 8 estructuras

--- clase8estructuras/main.c
+++ clase8estructuras/main.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define TAM 3
 #define LONG 30
 
-/*void func(char * p, char q)
-{
-    * p = 'D';
-    q = 'C';
 
-}
-
-int main()
-{
-    char letra1= 'A';
-    char letra2 = 'B';
-    func(&letra1,letra2);
-
-    printf("letra 1 :%s  letra2:%s", &letra1, &letra2);
-
-    return 0;
-
-}*/
 
 typedef struct
 {
@@ -35,44 +19,106 @@ char email[50];
 
 void mostrarListadoDeAlumnos(eAlumno[], int);
 
+//void ordenarListadoDeAlumnos(eAlumno[], int);
+
 void  mostrarAlumno(eAlumno alumnito);
+
+
+
+
+float calcularPromedio(int nota1, int nota2);
 
 
 int main()
 {
-    eAlumno miAlumno = {30, "Juan", 10, 6 ,8, "juan@gmail.com"};
+
     eAlumno listadoDeAlumnos[TAM];
+    eAlumno aux[TAM];
+    int valorstr;
 
-    int i;
 
-    for(i=0; i<TAM; i++)
+
+
+
+
+     /*int i;
+
+   for(i=0; i<TAM; i++)
     {
-        printf("Ingrese legajo");
+        printf("Ingrese legajo: ");
         scanf("%d", &listadoDeAlumnos[i].legajo);
 
-        printf("Ingrese legajo");
-        scanf("%d", &listadoDeAlumnos[i].legajo);
-
-        printf("Ingrese nombre");
+        printf("Ingrese nombre: ");
         fflush(stdin);
         gets(listadoDeAlumnos[i].nombre); // es por referencia no lleva &
 
-        printf("Ingrese legajo");
+        printf("Ingrese Nota1: ");
         scanf("%d", &listadoDeAlumnos[i].nota1);
 
-        printf("Ingrese legajo");
+        printf("Ingrese Nota2: ");
         scanf("%d", &listadoDeAlumnos[i].nota2);
 
-        printf("Ingrese mail");
+        printf("Ingrese mail: ");
         fflush(stdin);
         gets(listadoDeAlumnos[i].email); // es por referencia no lleva &
 
-        listadoDeAlumnos[i].promedio = (listadoDeAlumnos[i].nota1, listadoDeAlumnos[i].nota2);
+        listadoDeAlumnos[i].promedio = calcularPromedio(listadoDeAlumnos[i].nota1, listadoDeAlumnos[i].nota2);
     }
+*/
+
+     //Hardcodeo
+
+    fflush(stdin);
+    listadoDeAlumnos[1].legajo = 2;
+    strcpy(listadoDeAlumnos[1].nombre, "daniela");
+    listadoDeAlumnos[1].nota1 = 5;
+    listadoDeAlumnos[1].nota2 = 5;
+    strcpy(listadoDeAlumnos[1].email, "Mercedes@gamil");
+    listadoDeAlumnos[1].promedio = calcularPromedio(listadoDeAlumnos[1].nota1, listadoDeAlumnos[1].nota2);
+
+
+    fflush(stdin);
+    listadoDeAlumnos[0].legajo = 1;
+    strcpy(listadoDeAlumnos[0].nombre, "biviana");
+    listadoDeAlumnos[0].nota1 = 10;
+    listadoDeAlumnos[0].nota2 = 10;
+    strcpy(listadoDeAlumnos[0].email, "Maria@gamil");
+    listadoDeAlumnos[0].promedio = calcularPromedio(listadoDeAlumnos[0].nota1, listadoDeAlumnos[0].nota2);
+
+
+    fflush(stdin);
+    listadoDeAlumnos[2].legajo = 3;
+    strcpy(listadoDeAlumnos[2].nombre, "celeste");
+    listadoDeAlumnos[2].nota1 = 6;
+    listadoDeAlumnos[2].nota2 = 6;
+    strcpy(listadoDeAlumnos[2].email, "Bigi@gamil");
+    listadoDeAlumnos[2].promedio = calcularPromedio(listadoDeAlumnos[0].nota1, listadoDeAlumnos[0].nota2);
+
+
+    valorstr = strcmp(listadoDeAlumnos[1].nombre,listadoDeAlumnos[2].nombre);
+    printf("%d", valorstr);
+
+
+
+    //ordenar por legajo
+    for(int i=0; i<TAM-1; i++)
+    {
+        for(int j=i+1; j<TAM; j++)
+        {
+            if((strcmp(listadoDeAlumnos[i].nombre,listadoDeAlumnos[j].nombre))>0)
+            //if (listadoDeAlumnos[i].nombre<listadoDeAlumnos[j].legajo) // depende como lo ordeno va mayo o menor
+            {
+                aux[i]= listadoDeAlumnos[i];
+                listadoDeAlumnos[i]=listadoDeAlumnos[j];
+                listadoDeAlumnos[j]=aux[i];
+            }
+        }
+    }
+
 
     mostrarListadoDeAlumnos(listadoDeAlumnos, TAM);
 
-    //printf(%p %p")
+
     return 0;
 
 
@@ -92,10 +138,15 @@ void mostrarListadoDeAlumnos(eAlumno lista[], int cantidad)
 
 
 
+
+
+
 void  mostrarAlumno(eAlumno alumnito)
 {
-   printf("%d--, %s--, %d--, %d--, %f--\n",alumnito.legajo, alumnito.nombre, alumnito.nota1, alumnito.nota2, alumnito.promedio,alumnito.email );
+   printf("%d--, %s--, %d--, %d--, %f--, %s\n",alumnito.legajo, alumnito.nombre, alumnito.nota1, alumnito.nota2, alumnito.promedio, alumnito.email );
 }
+
+
 
 
 float calcularPromedio(int nota1, int nota2)
