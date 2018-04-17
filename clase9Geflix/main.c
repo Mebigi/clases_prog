d20a13712868f280368476b317dbb7fae178ec78
Mercedes Bigi <mebigi@hotmail.com>
2018-04-17 14:57:57 -0300

    avance con Gerflix

--- clase9Geflix/main.c
+++ clase9Geflix/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <conio.h>
+#define TAM 4
+#define LONG 20
 
 typedef struct
 {
 int idUsuario;
-char[] email[LONG];
-char[] Password;
+char email[LONG];
+char Password[LONG];
 int idSerie;
 int estado;
 } eUsuario;
@@ -14,18 +16,25 @@ int estado;
 typedef struct
 {
 int idSerie;
-char[] nombre;
+char nombre[LONG];
 int canttemporadas;
 int aniolanzamiento;
-int estado
+int estado;
 } eSerie;
 
+
+int IngresarOpcionMenu(int, int);
+int validarOpcionMenu(int, int, int);
+void menuSerie(eSerie [], int);
+int buscarLibre(eSerie [],int);
 
 
 int main()
 {
-    idUsuario listaUsuarios[TAM];
-    idSerie listaSerie[TAM];
+    //eUsuario listaUsuarios[TAM];
+    eSerie listaSerie[TAM];
+
+    menuSerie(listaSerie, TAM);
 
 
 
@@ -34,3 +43,95 @@ int main()
 
     return 0;
 }
+
+
+
+int IngresarOpcionMenu(int min, int max)
+{
+    int entero;
+
+    fflush(stdin);
+    scanf("%d",&entero);
+    entero = validarOpcionMenu(entero, min, max);
+
+    return entero;
+}
+
+
+
+
+int validarOpcionMenu (int dato, int min, int max)
+{
+    while(dato < min || dato > max)
+    {
+
+        fflush(stdin);
+        printf("Dato no v%clido reingresar:\n", 160);
+        scanf("%d", &dato);
+
+    }
+
+    return dato;
+
+}
+
+
+void menuSerie(eSerie lista[], int tam)
+{
+        int opcion;
+        int index;
+
+        do{
+                fflush(stdin);
+                system("cls");
+                printf("1 - ALTAS\n");
+                printf("2 - BAJAS\n");
+                printf("3 - MODIFICACIONES\n");
+                printf("4 - MOSTRAR\n");
+                printf("5 - ORDENAR\n");
+                printf("9 - SALIR\n");
+
+                opcion = IngresarOpcionMenu(1,9);
+
+                switch(opcion){
+                    case 1:
+                        index=buscarLibre(lista,TAM);
+                        if(index!=-1){
+                            lista[index].idSerie = index+1;
+                            fflush(stdin);
+                            printf("\nIngrese Nombre: ");
+                            gets(lista[index].nombre);
+                            fflush(stdin);
+                            printf("\nIngrese Cantidad de Temporadas: ");
+                            scanf("%d",&lista[index].canttemporadas);
+                            fflush(stdin);
+                            printf("\nIngrese Año de Lanzamiento: ");
+                            scanf("%d",&lista[index].aniolanzamiento);
+                            lista[index].estado = 1;
+
+                        }else{
+                            printf("No hay mas espacio para guardar series!!!");
+                            getche();
+                        }
+                        break;
+
+
+                }
+            }while(opcion!=9);
+
+}
+
+int buscarLibre(eSerie vec[],int tam)
+{
+    int index = -1;
+
+    for(int i=0;i<tam;i++){
+        if(vec[i].idSerie==0){
+            index=i;
+            break;
+        }
+    }
+
+     return index;
+}
+
